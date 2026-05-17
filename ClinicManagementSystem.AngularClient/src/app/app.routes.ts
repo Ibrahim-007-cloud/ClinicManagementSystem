14857f7ffc6c72c8b867f27a787a7105b0585907
Ibrahim <2504521@students.au.edu.pk>
2026-05-17 11:51:21 +0500

    Frontend: Initialized Angular client and routed views layout

--- ClinicManagementSystem.AngularClient/src/app/app.routes.ts
+++ ClinicManagementSystem.AngularClient/src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PatientListComponent } from './components/patient-list/patient-list.component';
+import { PatientFormComponent } from './components/patient-form/patient-form.component';
+import { PatientDetailsComponent } from './components/patient-details/patient-details.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'patients', pathMatch: 'full' },
+  { path: 'patients', component: PatientListComponent },
+  { path: 'patients/new', component: PatientFormComponent },
+  { path: 'patients/edit/:id', component: PatientFormComponent },
+  { path: 'patients/:id', component: PatientDetailsComponent },
+  { path: '**', redirectTo: 'patients' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
